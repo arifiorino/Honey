ad2b27875148883eb4b203575da9b01b9604295a
Dgomnbeault <69802906+Dgomnbeault@users.noreply.github.com>
2021-06-18 11:40:43 -0400

    lights and camera

--- shapesML.py
+++ shapesML.py
@@ -6,27 +6,18 @@ import cv2
 import sys
 
 motors = serial.Serial(port='COM3', baudrate = 250000)
+lights = serial.Serial(port='COM4', baudrate = 9600)
 time.sleep(2)
 print('connected')
 
-def getZ(x,y):
-  x1,y1=0,0
-  x2=190
-  y2=235
-  Q11=0
-  Q21=-1.3
-  Q22=-1.3
-  Q12=-0.6
-  A=np.array([x2-x,x-x1])
-  B=np.array([[Q11,Q12],[Q21,Q22]])
-  C=np.array([y2-y,y-y1]).T
-  return (1/((x2-x1)*(y2-y1))) * (A@B@C)
+def lightsOn():
+  lights.write(b'210101010255\n')
 
 def cmd(x):
   if x[0]=='E':
     motors.write(bytes('G1 %s F3.0\n'%x, 'utf-8'))
   else:
-    motors.write(bytes('G0 %s F300\n'%x, 'utf-8'))
+    motors.write(bytes('G0 %s F900\n'%x, 'utf-8'))
 
 def printShape(points, extrusions):
   prev = points[0]
@@ -38,14 +29,14 @@ def printShape(points, extrusions):
     time.sleep(0.1)
     prev = curr
 #Camera
-#Z+50
 #X-62.5
 #Y-9
-cameraPos = (-62.5,-9,50)
+#Z+50
+cameraPos = (-64,-9.5,50)
 motors.write(b'M17\n') #re enable steppers
 motors.write(b'G91\n') #relative motion mode
 time.sleep(5)
-points =           [(0,0),(3,0),(6,0),(9,0),(9,3),(9,6),(9,9),(6,9),(3,9),(0,9),(0,6),(0,3) ]
+points =           [(0,0),(2.66,0),(5.33,0),(8,0),(8,2.66),(8,5.33),(8,8),(5.33,8),(2.66,8),(0,8),(0,5.33),(0,2.66)]
 #Good:
 extrusions = [0.002, 0.01, 0.00, 0.00, 0.01, 0.00, 0.00,  0.01,  0.00, 0.00, 0.01, 0.00, 0.00]
 
@@ -79,13 +70,21 @@ def takePicture():
   frame = np.reshape(array,(height.value, width.value, bytes_per_pixel))
   frame = cv2.resize(frame,(0,0),fx=0.5, fy=0.5)
   #cv2.imshow("SimpleLive_Python_uEye_OpenCV", frame)
+  #cv2.imwrite("C:\\Users\\AM ARES 2\\Desktop\\img.png", frame)
+
+  print(height.value, width.value)
+  cv2.imwrite("C:\\Users\\AM ARES 2\\Desktop\\img.png", frame)
+  input('1')
   ueye.is_FreeImageMem(hCam, pcImageMemory, MemID)
   ueye.is_ExitCamera(hCam)
   #cv2.destroyAllWindows()
   return frame
-  
+
+lightsOn()
+print(takePicture())
 while 1:
   cmd(input('?'))
+
 for _ in range(1):
   for _ in range(1):
     #extrusions=np.random.rand(len(extrusions))*0.015
